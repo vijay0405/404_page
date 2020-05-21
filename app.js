8ae25fcde866d074177596811d24d79fe7138db4
vijay0405 <uvijaysimha@gmail.com>
2020-05-22 00:10:33 +0530

    refactored

---
+++ app.js
@@ -0,0 +1,8 @@
+const icon = document.getElementById("icon")
+icon.addEventListener("mouseenter", function (event) {
+    icon.style.color = '#7a5901'
+})
+
+icon.addEventListener("mouseleave", function (event) {
+    icon.style.color = '#7B341E'
+})
